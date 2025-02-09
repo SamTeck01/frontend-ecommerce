f1dc0b2dcd59909b0b2f41f1d72b9684509ad812
samteck <samadoye28@gmail.com>
2025-02-09 23:25:54 +0100

    fixing err

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,9 +1,9 @@
 
-import Hero from "./Hero";
-import Popular from "./Popular";
-import Offer from "./Offer";
-import NewCollections from "./NewCollections";
-import NewsLetter from "./NewsLetter";
+import Hero from "./Hero.jsx";
+import Popular from "./Popular.jsx";
+import Offer from "./Offer.jsx";
+import NewCollections from "./NewCollections.jsx";
+import NewsLetter from "./NewsLetter.jsx";
 
 export default function Home() {
   return (
