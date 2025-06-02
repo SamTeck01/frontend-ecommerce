eff67950a668facc3e1ec9b85803788ccd6564ac
samteck <samadoye28@gmail.com>
2025-06-02 08:27:24 +0100

    commit

--- src/pages/Footer.jsx
+++ src/pages/Footer.jsx
@@ -7,7 +7,7 @@ import SOCIALS from '../assets/socials';
 function FooterColumn ({title, children, className}){
   return(
     <div className="flex flex-col gap-2">
-      <h4 className="bold-18 whitespace-nowrap" >{title} </h4>
+      <h4 className="bold-18 whitespace-nowrap text-white" >{title} </h4>
       <div className={className} >{children}</div>
     </div>
   )
@@ -19,15 +19,15 @@ export default function Footer() {
       <footer className="flexCenter pb-20 pt-20" >
         <div className="max_padd_container flex w-full flex-col gap-14" >
           <div className="flex flex-col items-start justify-center gap-[10%] md:flex-row " >
-            <Link to='/' className="mb-10 bold-20 ">Shoppee</Link>
+            <Link to='/' className="mb-10 bold-20 text-white">Shoppee</Link>
 
             <div className="flex flex-wrap gap-8 sm:justify-between md:flex-1" >
               {FOOTER_LINKS.map((col) => {
                 
                 return(<>
-                  <FooterColumn title={col.title} key={FOOTER_LINKS.length ++} className={"flex flex-col gap-4 regular-14 text-gray-20" } >
+                  <FooterColumn title={col.title} key={FOOTER_LINKS.length ++} className={"flex flex-col gap-4 regular-14 text-ash" } >
                     {col.links.map((link) => <ul key={col.title} >
-                      <Link to={'/'} key={link}> {link} </Link>
+                      <Link to={'/'} key={link} className="" > {link} </Link>
                     </ul> ) }
                   </FooterColumn>
                 </>)
@@ -36,7 +36,7 @@ export default function Footer() {
                 <FooterColumn title={FOOTER_CONTACT_INFO.title} className={"flex gap-4 md:flex-col lg:flex-row"} >
                   {FOOTER_CONTACT_INFO.links.map(link => 
                     <Link to={'/'} key={link.label}>
-                      <p className="mb-2">{link.label}:</p> <p className="medium-14" >{link.value}</p>
+                      <p className="mb-2 text-white">{link.label}:</p> <p className="medium-14 text-ash" >{link.value}</p>
                     </Link>
                   ) }
                 </FooterColumn> 
@@ -54,8 +54,8 @@ export default function Footer() {
               </div>
             </div>
           </div>
-          <div className="border bg-gray-20 "></div>
-          <p className="text-center regular-14 text-gray-30">2024 Shoppee | All right reserved</p>
+          <div className="border bg-ash"></div>
+          <p className="text-center regular-14 text-ash">2024 Shoppee | All right reserved</p>
         </div>
       </footer>
     </div>
